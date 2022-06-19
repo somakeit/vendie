5dd880d1857538656d5f0e5b809277254b7c5691
PhilMinard <pminard1@outlook.com>
2022-06-19 14:53:46 +0100

    Added command numbers

--- vendor/commands.py
+++ vendor/commands.py
@@ -2,4 +2,9 @@ from enum import Enum
 
 
 class Command(Enum):
-    pass
+    RESET = '0x1060'
+    SETUP = '0x1161'
+    POLL = '0x1262'
+    VEND = '0x1363'
+    READER = '0x1464'
+    EXPANSION = '0x1767'
