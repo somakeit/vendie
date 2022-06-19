9b5c9810f500fef404fcf2cd197127473d9989c4
sam57719 <sam57719@gmail.com>
2022-06-19 14:29:04 +0100

    development

--- vendor/states.py
+++ vendor/states.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
 
 class State(Enum):
@@ -11,7 +13,48 @@ class State(Enum):
     
 
 
+class BaseState(ABC):
+    name: State
+
+    @abstractmethod
+    def run(self):
+        pass
+
+
+class Inactive(BaseState):
+    name = State.INACTIVE
+
+    def run(self):
+        pass
+
+
+class Disabled(BaseState):
+    name = State.DISABLED
+
+    def run(self):
+        pass
+
+
+class Enabled(BaseState):
+    name = State.ENABLED
+
+    def run(self):
+        pass
+
+
+class SessionIdle(BaseState):
+    name = State.SESSION_IDLE
+
+    def run(self):
+        pass
+
+
+class Vend(BaseState):
+    name = State.VEND
+
+    def run(self):
+        pass
+
+
 if __name__ == '__main__':
     print([state for state in list(State)])
-
-    print(State.INACTIVE)
