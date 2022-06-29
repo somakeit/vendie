7d475a4b6ff71e1a5cb557017fe306d08aca1eb9
sam57719 <sam57719@gmail.com>
2022-06-29 19:55:26 +0100

    Development

--- vendor/functions.py
+++ vendor/functions.py
@@ -2,10 +2,14 @@ import serial
 from serial.tools import list_ports
 
 
-def get_serial_device_from_description(description: str):
-    return next((port.description for port in list(list_ports.comports()) if port.description == description), None)
+def get_serial_device_from_description(description: str, baudrate: int = 9600, timeout: int = 10) -> serial.Serial:
+    device = next((port.device for port in list(list_ports.comports()) if port.description == description), None)
+    if device is None:
+        return None
+    return serial.Serial(device, baudrate, timeout=timeout)
 
 
 if __name__ == '__main__':
-    test = serial.Serial(get_serial_device_from_description('test'), 9600, 10)
+    print([port.description for port in list(list_ports.comports())])
+    test = get_serial_device_from_description('test')
     print(test)
--- vendor/statemachine.py
+++ vendor/statemachine.py
@@ -1,5 +1,6 @@
 from .states import State, build_state_map, BaseState
 from .config import PORT_DESCRIPTIONS
+from .functions import get_serial_device_from_description
 import serial
 
 
@@ -13,8 +14,16 @@ class CashlessDevice:
         self._init_devices()
 
     def _init_devices(self):
-        # self.card_reader = [serial.Serial(port.device) for port in list_ports.comports()]
-        pass
+        self.card_reader = get_serial_device_from_description(PORT_DESCRIPTIONS['card_reader'])
+        self.vendor = get_serial_device_from_description(PORT_DESCRIPTIONS['vendor'])
+
+        print(f'Card Reader ({PORT_DESCRIPTIONS["card_reader"]})'
+              f' {"found" if self.card_reader is not None else "not found!"}')
+        print(f'Card Reader ({PORT_DESCRIPTIONS["vendor"]})'
+              f' {"found" if self.card_reader is not None else "not found!"}')
+
+
+
 
     def start(self):
         while True:
