2ba5c6eaa616a40c410344cbb5734545b0c0cddc
sam57719 <sam57719@gmail.com>
2022-06-29 22:11:47 +0100

    Development

--- vendor/states.py
+++ vendor/states.py
@@ -52,7 +52,7 @@ class Inactive(BaseState):
                 command = self.vendor.read_until(b'\x03')
                 # command_str = command.hex().upper()
                 command_ascii = command.decode('ascii')
-                command_str = str(command)
+                command_str = str(command_ascii)
                 print(f'{command=}')
                 print(f'{command_ascii=}')
                 print(f'{command_str=}')
