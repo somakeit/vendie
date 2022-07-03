1ff9e4fdc2acbd8a75ce0203e996e23849b6a938
sam57719 <sam57719@gmail.com>
2022-07-03 16:39:29 +0100

    Testing Reader_disable

--- vendor/states.py
+++ vendor/states.py
@@ -86,7 +86,7 @@ class Disabled(BaseState):
         while True:
             command_str = self._state_machine.read_command()
             command = Command.find_command(command_str)
-            print(command)
+            print(command, f'{command_str=}')
             if DEBUG:
                 print(f'{command_str=}')
                 print(f'{command=}')
