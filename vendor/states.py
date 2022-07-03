1cbc02927d6aa24a8cf24f98b39f33c2f18dbf6f
sam57719 <sam57719@gmail.com>
2022-07-03 16:34:14 +0100

    Testing Reader_disable

--- vendor/states.py
+++ vendor/states.py
@@ -95,7 +95,7 @@ class Disabled(BaseState):
                 case Command.READER_ENABLE:
                     break
                 case Command.EXPANSION_REQUEST_ID:
-                    self._state_machine.send_response(Response.PERIPHERAL_ID + '00'*30)
+                    self._state_machine.send_response(Response.PERIPHERAL_ID + '00'*39)
                 case Command.RESET:
                     return State.INACTIVE
                 case Command.READER_DISABLE:
