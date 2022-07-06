1b35cae836ba617fba9dd114b271e86ab933114a
sam57719 <sam57719@gmail.com>
2022-07-06 20:51:14 +0100

    Testing Reader_disable

--- vendor/api.py
+++ vendor/api.py
@@ -3,19 +3,31 @@ from abc import ABC, abstractmethod
 
 class Api(ABC):
     """Testing card response base"""
+
+    @property
+    @abstractmethod
+    def API_ENDPOINT(self):
+        raise NotImplementedError
+
     @abstractmethod
     def validate_card(self, data: dict) -> bool:
         pass
+
 
 class TestApi(Api):
     """Sub class"""
 
     API_ENDPOINT = "https://dev.c38.co/smi/vend_api.php"
+
     def validate_card(self, data: dict) -> bool:  # dict = data to send method
         pass
 
 
 class RealApi(Api):
     """Sub class"""
+
     def validate_card(self, data: dict) -> bool:  # dict = data to send method
         pass
+
+if __name__ == '__main__':
+    api = TestApi()
