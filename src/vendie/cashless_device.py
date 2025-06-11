c981cffb923833424d50c3790be201b48a317c4a
sam57719 <sam57719@gmail.com>
2025-06-11 22:00:13 +0100

    Update READER_SETUP_DATA configuration constant
    Modified the READER_SETUP_DATA value to align with the latest protocol requirements. This ensures compatibility with updated system specifications and prevents potential operational issues.

--- src/vendie/cashless_device.py
+++ src/vendie/cashless_device.py
@@ -25,7 +25,7 @@ class State(StrEnum):
 
 class CashlessDevice:
 
-    READER_SETUP_DATA: str = "0102203201020210"
+    READER_SETUP_DATA: str = "0102203201020201"
 
     def __init__(self, vending_machine: Serial, card_reader: Serial):
         self.vending_machine: Serial = vending_machine
